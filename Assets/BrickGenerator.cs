0b364087baac39a3e9dbb274351bc50a2c1caf9a
Mitsuhiro Koga <shiena.jp@gmail.com>
2017-01-07 03:37:55 +0900

    Dynamic generate BrickPrefab

---
+++ Assets/BrickGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickGenerator : MonoBehaviour
+{
+
+    public GameObject brick;
+
+    public Vector3 StartPosition;
+
+    public int Count = 29;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int index = 0; index < Count; index++)
+        {
+            Instantiate(brick, StartPosition + Vector3.left * index, Quaternion.identity);
+        }
+    }
+}
